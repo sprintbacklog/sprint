3d632c9ccd2f4be1983dce8dfe64878bb0b33ae2
AndresMiraQ <krhistian1902@gmail.com>
2021-09-22 20:07:13 -0500

    Cambios a lo estilos y validacion de contraseña

---
+++ form.js
@@ -0,0 +1,22 @@
+//const val = document.getElementById('clave').value;
+
+//const val = "asd(12*D";
+const checkContrasena = (valor) => {
+    if(valor != ""){
+        //Validación usando expresiones regulares
+        let validation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        if(validation.test(valor)){
+            console.log(valor + " valido");
+            return true;
+        }else{
+            console.log(valor + " no valido");
+            return false;
+        }
+    }else{
+        console.log(valor + " no valido laksalks");
+        return false;
+    }  
+};
+checkContrasena(val);
+
+//module.exports = {checkContrasena};
